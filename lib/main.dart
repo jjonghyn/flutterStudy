abfaad0d1e7c31e0b1f80442913473b66da05388
jjonghyn <jjonghyunn0802@gmail.com>
2023-05-02 17:16:30 +0900

    강의 첫번째 예제 i am rich 어플 만들기 끝

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Center(child: Text('hello World'),)));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png')
+          ),
+        ),
+      ),
+    ),
+  );
 }
-
